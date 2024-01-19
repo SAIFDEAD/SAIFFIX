88dd0771d132323498449829735661598b6b7997
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-01-20 03:04:09 +0530

    𝐍αяυтσ ✘ 𝐑σвσт

--- DAXXROBOT/__init__.py
+++ DAXXROBOT/__init__.py
@@ -52,7 +52,7 @@ if ENV:
         "START_IMG", ""
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "worldwide_friend_zone")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "SAIFHELPGC")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -70,7 +70,7 @@ if ENV:
 
     try:
         DRAGONS = set(int(x) for x in os.environ.get("DRAGONS", "").split())
-        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "2145093972").split())
+        DEV_USERS = set(int(x) for x in os.environ.get("DEV_USERS", "6585111267").split())
     except ValueError:
         raise Exception("Your sudo or dev users list does not contain valid integers.")
 
@@ -160,7 +160,7 @@ pbot = Client("DAXXROBOT", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN,in_
 dispatcher = updater.dispatcher
 aiohttpsession = ClientSession()
 
-print("[INFO]: Getting Bot Info...")
+print("[INFO]: 𝙶𝚎𝚝𝚝𝚒𝚗𝚐 𝙱𝚘𝚝 𝙸𝚗𝚏𝚘..💖.")
 BOT_ID = dispatcher.bot.id
 BOT_NAME = dispatcher.bot.first_name
 BOT_USERNAME = dispatcher.bot.username
