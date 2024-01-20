d2cb52d28078d6bcdbf8683704847b58b3aa85a3
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-01-21 05:24:33 +0530

    𝐍αяυтσ ✘ 𝐑σвσт

--- DAXXROBOT/modules/alive.py
+++ DAXXROBOT/modules/alive.py
@@ -10,21 +10,25 @@ from telethon import __version__ as tver
 from DAXXROBOT import SUPPORT_CHAT, pbot,BOT_USERNAME, OWNER_ID,BOT_NAME,START_IMG
 
 PHOTO = [
-    "https://telegra.ph/file/d2a23fbe48129a7957887.jpg",
-    "https://telegra.ph/file/ddf30888de58d77911ee1.jpg",
-    "https://telegra.ph/file/268d66cad42dc92ec65ca.jpg",
-    "https://telegra.ph/file/13a0cbbff8f429e2c59ee.jpg",
-    "https://telegra.ph/file/bdfd86195221e979e6b20.jpg",
+    "https://telegra.ph/file/a4c4f2bcf8e7d3d3ac6b0.jpg",
+    "https://telegra.ph/file/8373b5620619404bd49d6.jpg",
+    "https://telegra.ph/file/db895af6e677fc427a2c8.jpg",
+    "https://telegra.ph/file/7d4fe83b385614fb56929.jpg",
+    "https://telegra.ph/file/c14176d8c683700fab36e.jpg",
+    "https://telegra.ph/file/0b9c857ff8280be188e11.jpg",
+    "https://telegra.ph/file/c92b91e44e9174f083448.jpg",
+    "https://telegra.ph/file/6d9c63e666615553d6bec.jpg",
+    "https://telegra.ph/file/ee8ac17daa63d59d36e8c.jpg"
 ]
 
 Mukesh = [
     [
-        InlineKeyboardButton(text="ɴᴏᴏʙ", user_id=OWNER_ID),
-        InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
+        InlineKeyboardButton(text="𝐎𝚆𝙽𝙴𝚁", user_id=OWNER_ID),
+        InlineKeyboardButton(text="𝐒ᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
     ],
     [
         InlineKeyboardButton(
-            text="➕ᴀᴅᴅ ᴍᴇ ᴇʟsᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ➕",
+            text="𝐀ᴅᴅ ᴍᴇ ʙᴀʙʏ ",
             url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
         ),
     ],
@@ -32,7 +36,7 @@ Mukesh = [
 
 
 
-@pbot.on_message(filters.command("alive"))
+@pbot.on_message(filters.command("alive "))
 async def restart(client, m: Message):
     await m.delete()
     accha = await m.reply("⚡")
@@ -54,7 +58,7 @@ async def restart(client, m: Message):
         START_IMG,
         caption=f"""**ʜᴇʏ, ɪ ᴀᴍ 『[{BOT_NAME}](f"t.me/{BOT_USERNAME}")』**
    ━━━━━━━━━━━━━━━━━━━
-  » **ᴍʏ ᴏᴡɴᴇʀ :** [ᴏᴡɴᴇʀ](tg://user?id={OWNER_ID})
+  » **ᴍʏ ᴏᴡɴᴇʀ :** [𝐎ᴡɴᴇʀ](tg://user?id={OWNER_ID})
   
   » **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{lver}`
   
