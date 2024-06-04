8f5264d4bcb9ec9a3c931183aba552d0d1f29b27
SAIFDEAD <145872388+SAIFDEAD@users.noreply.github.com>
2024-06-04 12:51:14 +0530

    Update and rename __main__ to __main__.py

---
+++ DAXXROBOT/__main__.py
@@ -0,0 +1,51 @@
+import importlib
+import re
+import time
+import asyncio
+from platform import python_version as y
+from sys import argv
+from pyrogram import __version__ as pyrover
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from telegram import __version__ as telever
+from telegram.error import (
+    BadRequest,
+    ChatMigrated,
+    NetworkError,
+    TelegramError,
+    TimedOut,
+    Unauthorized,
+)
+from telegram.ext import (
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+)
+from telegram.ext.dispatcher import DispatcherHandlerStop
+from telegram.utils.helpers import escape_markdown
+from telethon import __version__ as tlhver
+
+import DAXXROBOT.modules.no_sql.users_db as sql
+from DAXXROBOT import (
+    BOT_NAME,
+    BOT_USERNAME,
+    LOGGER,
+    OWNER_ID,
+    START_IMG,
+    SUPPORT_CHAT,
+    TOKEN,
+    StartTime,
+    dispatcher,
+    pbot,
+    telethn,
+    updater,
+)
+from DAXXROBOT.modules import ALL_MODULES
+
+
+if __name__ == "__main__":
+    LOGGER.info("Successfully loaded naruto robot modules: " + str(ALL_MODULES))
+    telethn.start(bot_token=TOKEN)
+    pbot.start()
+  
